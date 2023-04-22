e499eb1d9f951655d8b6fe1301cc75c68fd7673c
Phạm Công Tính <84382773+TinhSoMa@users.noreply.github.com>
2023-04-22 23:48:31 +0700

    22/04/2023

--- app/src/main/java/com/example/music/service/APIService.kt
+++ app/src/main/java/com/example/music/service/APIService.kt
@@ -1,7 +1,9 @@
 package com.example.music.service
 
 import com.example.music.model.Ads
+import com.example.music.model.Album
 import com.example.music.model.PlayList
+import com.example.music.model.TopicCategory
 import retrofit2.Call
 import retrofit2.http.GET
 
@@ -11,4 +13,10 @@ interface APIService {
 
     @GET("Service/select_playlist.php")
     fun getDataPlayList(): Call<List<PlayList>>
+
+    @GET("Service/select_topic_category.php")
+    fun getDataTopicCategory(): Call<TopicCategory>
+
+    @GET("Service/select_album.php")
+    fun getDataAlbum(): Call<List<Album>>
 }
